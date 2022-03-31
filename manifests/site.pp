5a3d0bd5c90a3593d873e920af1e2b2314960402
Fenikks <49687341+Fenikks@users.noreply.github.com>
2022-03-31 22:22:35 +0300

    added group

--- manifests/site.pp
+++ manifests/site.pp
@@ -15,6 +15,9 @@ class dev_user(
   $pswd = '$1$Gdi1eg83$lCTP35NrycMk.MO7WQ5Ut1',
   $grps = ['wheel']
 ){
+  group {$grps:
+    ensure => present
+  }
   user {$usrnm:
     ensure => present,
     managehome => true,
