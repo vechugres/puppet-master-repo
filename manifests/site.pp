5288c3bd0b4f6a6233fa01a19a30d2102bb188c6
Fenikks <49687341+Fenikks@users.noreply.github.com>
2022-03-31 21:05:11 +0300

    added classes

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,27 @@
 node default{
- file { '/root/README':
-   ensure => file,
-   content => 'Hello, World!',
- }
+  file { '/root/README':
+    ensure => file,
+    content => 'Hello, World!',
+  }
+  include dev_user
+  include dev_editor
+}
+
+class dev_user {
+  user {'demouser':
+    ensure => present
+    managehome => true
+    group => ['wheel']
+    password => '$1$Gdi1eg83$lCTP35NrycMk.MO7WQ5Ut1'
+  }
+}
+
+class dev_editor {
+  package {'vim':
+    ensure => present
+  }
+  file {'/home/demouser/.vimrc':
+    ensure => file
+    content => 'puppet:///modules/dev_environment/vimrc'
+  }
 }
