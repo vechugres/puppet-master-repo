a02f954e814b7441ce1cbd7cb540aedcb9029fa1
Alina Freydina <alina.freydina@gmail.com>
2022-04-01 23:00:18 +0300

    added slave2

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,7 +7,7 @@ node 'slave2.puppet'{
   package { ['httpd', 'php']:
     provider => yum,
     ensure => isntalled,
-    install_options => ['-y'],
+    install_options => ['-d 10'],
 
   }
   #include role::web_server
