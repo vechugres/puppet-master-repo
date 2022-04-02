accaba90e11af3b24b399fb70cbcfa99a1b5fee0
Fenikks <49687341+Fenikks@users.noreply.github.com>
2022-04-02 10:37:44 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,31 @@
 node default{
  file { '/root/README':
    ensure => file,
-  }
    content => 'Hello, World!',
  }
+ 
+ include dev_user
+ include dev_editor
+} 
+
+
+class dev_user {
+   user {'demouser':
+     ensure => present,
+     managehome => true,
+     groups => ['wheel'],
+     password => '$1$Gdi1eg83$lCTP35NrycMk.MO7WQ5Ut1'
+   }
+}
+
+class dev_editor {
+  package{'vim':
+    ensure => present
+  }
+  file {'/home/demouser/.vimrc':
+    ensure => file,
+    owner => 'demouser',
+    group => 'demouser',
+    content => 'set number'
+  }
 }
