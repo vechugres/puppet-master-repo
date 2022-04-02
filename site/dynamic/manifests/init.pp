7e01402be2b0c5d2b53b6ce0745080c44c424faf
Fenikks <49687341+Fenikks@users.noreply.github.com>
2022-04-02 12:10:03 +0300

    Create init.pp

---
+++ site/dynamic/manifests/init.pp
@@ -0,0 +1,20 @@
+class dynamic{
+  package{['httpd', 'php']:
+    ensure => installed
+  }
+  file {'/var/www/php':
+    ensure => directory
+  }
+  file {'/var/www/php/index.php':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/index.php'
+  }
+  file{'/etc/httpd/conf.d/demo.conf':
+    ensure => file,
+    source => 'puppet:///modules/dynamic/demo.conf',
+    notify => Service['httpd']
+  }
+  service{'httpd':
+    ensure => running
+  }
+}
