54f070a4134143070babad44bff89939a4f58b66
Fenikks <49687341+Fenikks@users.noreply.github.com>
2022-04-02 12:30:58 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,19 +5,3 @@ node default{
 node /slave\d\.puppet/{
  include role::web_server
 }
-
-
-
-class dev_user_nemo (
-  $pswd = '$1$Gdi1eg83$lCTP35NrycMk.MO7WQ5Ut1',
-  $grps = ['wheel']
-){
-  class {'dev_user':
-    usrnm => 'nemo',
-    pswd => $pswd,
-    grps =>  $grps
-  }
-  
-  include dev_editor
-}
-
